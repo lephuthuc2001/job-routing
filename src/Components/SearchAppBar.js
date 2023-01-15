5aa60be1527c48c0babecd3e68d0bfc91a1e1362
Lê Phú Thức <95269623+lephuthuc2001@users.noreply.github.com>
2023-01-15 18:17:10 +0700

    Update SearchAppBar.js

--- src/Components/SearchAppBar.js
+++ src/Components/SearchAppBar.js
@@ -1,68 +1,135 @@
-import SearchAppBar from "./SearchAppBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import { Pagination } from "@mui/material";
-import { useState } from "react";
-import JobsList from "./JobsList";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button } from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
+import { Link } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useAuth } from "./Auth";
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Outlet } from "react-router-dom";
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
-import { jobData } from "../data/specificData";
-function Home() {
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
     },
-  });
-  // console.log(baseURL);
-  const [page, setPage] = useState(1);
-  const [query, setQuery] = useState("");
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
+  },
+}));
 
-  const searchHandler = (q) => {
-    setQuery(q);
-    setPage(1);
-  };
-
+export default function SearchAppBar({ onSearch }) {
+  const auth = useAuth();
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline enableColorScheme />
-      <div
-        className="App"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "30px",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <SearchAppBar onSearch={searchHandler} />
-        <JobsList
-          pageNum={page}
-          data={jobData.filter((job) =>
-            job.title.toLowerCase().includes(query.toLowerCase())
-          )}
-        />
-        <Pagination
-          page={page}
-          onChange={handleChange}
-          count={Math.ceil(
-            jobData.filter((job) =>
-              job.title.toLowerCase().includes(query.toLowerCase())
-            ).length / 6
-          )}
-          color="primary"
+    <Box sx={{ width: "100%" }}>
+      <AppBar position="static">
+        <Toolbar
           sx={{
-            marginTop: "20px",
+            display: "flex",
+            p: "0 50px",
+            justifyContent: "space-around",
+            gap: "20%",
           }}
-        />
-        <Outlet />
-      </div>
-    </ThemeProvider>
+        >
+          <div style={{ display: "flex", gap: "30px" }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexShrink: 1, display: { xs: "none", sm: "block" } }}
+            >
+              Job Hunt
+            </Typography>
+            <div style={{ display: "flex", gap: "5px" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
+                  onChange={(e) => {
+                    onSearch(e.target.value);
+                  }}
+                />
+              </Search>
+            </div>
+          </div>
+          {!auth.user ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <LoginIcon />
+              <Link
+                to="login"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                }}
+              >
+                <Button color="inherit">Login</Button>
+              </Link>
+            </div>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                onClick={() => {
+                  auth.logout();
+                }}
+              >
+                <LogoutIcon />
+              </Button>
+
+              <Typography color="inherit">{auth.name}</Typography>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default Home;
